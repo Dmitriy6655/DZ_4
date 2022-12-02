47b03962ba6d0c65cf2076e5550787e6d73e6a66
Timofey_V <tim_vi_ov@mail.ru>
2022-12-02 22:39:38 +0300

    add Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 27: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int result = SumDigits(num);
+Console.WriteLine($"Сумма цифр в числе равна: {result}");
+
+
+//МЕТОД сложения цифр в числе
+int SumDigits (int num)
+{
+    int sum = 0;
+    int temporary_variable = 0;
+
+    while (num > 0)
+    {
+        temporary_variable = num % 10;
+        num = num / 10;
+        sum = temporary_variable + sum;
+             
+    }
+    return sum;
+}
